e361d64f0a699f94c47eb196252bfdf8e780cedd
gabrielcampoos <gabrielcampoos@hotmail.com>
2024-11-14 20:42:43 -0300

    commit

--- src/app/features/file/controller/file.controller.ts
+++ src/app/features/file/controller/file.controller.ts
@@ -11,7 +11,6 @@ import {
   ListFilesUsecase,
   UpdateFileUsecase,
 } from "../usecase";
-import { exec } from "child_process";
 
 export class FilesController {
   public static async createFile(req: Request, res: Response) {
@@ -85,79 +84,6 @@ export class FilesController {
         res,
         Result.error(500, error.toString())
       );
-    }
-  }
-
-  // Função para fazer o upload
-  public static async uploadFiles(req: Request, res: Response) {
-    try {
-      // Verifica se arquivos foram enviados
-      if (!req.files || req.files.length === 0) {
-        return res.status(400).send("Nenhum arquivo enviado.");
-      }
-
-      // Aqui, você pode fazer qualquer processamento que precisa com os arquivos, como salvar no banco de dados
-      console.log("Arquivos carregados:", req.files);
-
-      // Retornar uma resposta de sucesso
-      return res.status(200).send("Arquivos carregados com sucesso.");
-    } catch (error) {
-      console.error("Erro ao fazer upload dos arquivos:", error);
-      return res.status(500).send("Erro ao processar o upload.");
-    }
-  }
-
-  // Função principal de download
-  static async downloadJonBet(req: Request, res: Response) {
-    console.log("Requisição recebida para download de JonBet.");
-    try {
-      const folderPath = "/tmp/JonBet"; // Caminho da pasta a ser compactada
-      const zipPath = path.join("/tmp", "JonBet.zip"); // Caminho onde o arquivo .zip será salvo
-
-      console.log("Verificando a existência da pasta:", folderPath);
-      if (!fs.existsSync(folderPath)) {
-        console.error("Pasta não encontrada no diretório /tmp");
-        return res.status(404).send("Pasta não encontrada.");
-      }
-
-      // Criando o arquivo .zip usando o Archiver
-      const output = fs.createWriteStream(zipPath);
-      const archive = archiver("zip", {
-        zlib: { level: 9 }, // Alta compressão
-      });
-
-      output.on("close", () => {
-        console.log("Arquivo ZIP criado com sucesso!");
-      });
-
-      archive.on("error", (err) => {
-        console.error("Erro ao criar o arquivo ZIP:", err);
-        return res.status(500).send("Erro ao criar o arquivo.");
-      });
-
-      archive.pipe(output);
-      archive.directory(folderPath, false); // Adiciona o conteúdo da pasta sem o nome da pasta
-      await archive.finalize(); // Finaliza a criação do arquivo
-
-      // Enviar o arquivo .zip para o cliente
-      res.setHeader("Content-Type", "application/zip");
-      res.download(zipPath, "JonBet.zip", (err) => {
-        if (err) {
-          console.error("Erro ao enviar o arquivo:", err);
-          return res.status(500).send("Erro ao enviar o arquivo.");
-        }
-        console.log("Arquivo enviado com sucesso.");
-
-        // Deletar o arquivo temporário após o download
-        fs.unlink(zipPath, (delError) => {
-          if (delError) {
-            console.error("Erro ao deletar o arquivo:", delError);
-          }
-        });
-      });
-    } catch (err) {
-      console.error("Erro no backend:", err);
-      return res.status(500).send("Erro ao processar o download.");
     }
   }
 }
--- src/app/features/file/file.routes.ts
+++ src/app/features/file/file.routes.ts
@@ -9,20 +9,8 @@ export default () => {
   const router = Router();
 
   router.post("/file", authMiddleware, FilesController.createFile);
-  router.post(
-    "/upload",
-    authMiddleware,
-    upload.array("files"),
-    FilesController.uploadFiles
-  );
 
   router.get("/file", authMiddleware, FilesController.listFiles);
-
-  router.get(
-    "/file/download-jonbet",
-    authMiddleware,
-    FilesController.downloadJonBet
-  );
 
   router.put("/file/:id", authMiddleware, FilesController.editFile);
   router.delete("/file/:id", authMiddleware, FilesController.deleteFile);
